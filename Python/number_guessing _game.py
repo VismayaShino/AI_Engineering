2a649b54a2d9dad9d991ee6e5ff356e1d52b65c6
Vismaya Shino <vismayashino2004@gmail.com>
2026-06-09 12:41:15 +0530

    Day 3 done

---
+++ Python/number_guessing _game.py
@@ -0,0 +1,19 @@
+secret_number = 7
+string = input("Enter you name: ")
+print("Hello "+ string + "!! Welcome to the number guessing game!")
+value = int(input("Enter your guess: "))
+count = 1
+while (secret_number != value):
+    if(value > secret_number):
+        print("Uff!! Too high")
+        value = int(input("Enter your guess: "))
+        count = count+1
+    elif(value < secret_number):
+        print("Ooops!! Too low")
+        value = int(input("Enter your guess: "))
+        count = count +1
+   
+print("Congatulations "+ string +"! You made it")
+print("Woohooo! You solved it in "+ str(count) +" attempts")
+
+
