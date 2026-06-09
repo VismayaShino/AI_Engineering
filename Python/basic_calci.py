87d59c22c0a23325d8fd5e1e1ed12b8ce380ab4f
Vismaya Shino <vismayashino2004@gmail.com>
2026-06-09 08:25:41 +0530

    New Changes made

---
+++ Python/basic_calci.py
@@ -0,0 +1,11 @@
+#basic calculator
+value1 = float(input("Enter value 1: "))
+value2 = float(input("Enter value 2: "))
+add = value1 + value2
+subtraction = value2 - value1
+multiply = value1*value2
+division = (value1/value2)
+print("Addition: ",add)
+print("Subtraction: ",subtraction)
+print("Multiply: ",multiply)
+print("Division: ",division)
